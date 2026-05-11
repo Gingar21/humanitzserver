e1c45cdc98860a89aa1d408f6ce7d40c645dd941
Gingar21 <seidezeus@gmail.com>
2026-05-11 18:03:12 +0200

    Add files via upload

---
+++ Start.bat
@@ -0,0 +1,39 @@
+@echo off
+:: --- CONFIGURATION ---
+:: If the server is not in the same folder, put the full path here.
+:: For REBOOT_TIME set your server time 
+:: log port=default is 77777  queryport=default is 27015"
+set "EXE_NAME=HumanitZServer.exe"
+set "ARGS=-log port=  queryport="
+set "REBOOT_TIME=00:00"
+:: ---------------------
+
+:start_server
+cls
+echo [%date% %time%] Server launch...
+start "" "%EXE_NAME%" %ARGS%
+
+:check_loop
+:: 60 SECOND WAIT
+timeout /t 60 /nobreak > nul
+
+:: 1. CRASH VERIFICATION
+tasklist /FI "IMAGENAME eq %EXE_NAME%" | find /I "%EXE_NAME%" > nul
+if errorlevel 1 (
+    echo [%time%] SERVEUR CRASH ! Restarting...
+    goto start_server
+)
+
+:: 2. TIME CHECK (HH:MM format)
+set "curtime=%time:~0,5%"
+set "curtime=%curtime: =0%"
+
+if "%curtime%"=="%REBOOT_TIME%" (
+    timeout /t 5 /nobreak > nul
+    :: --- RCON SCRIPT CALL ---
+    echo Sending the RCON backup command...
+    python humanitz1_rcon.py
+)
+
+:: Back to the loop
+goto check_loop
---
+++ humanitz_rcon.py
@@ -0,0 +1,40 @@
+import socket
+import struct
+import sys
+import time
+
+
+# CONFIGURATION 
+RCON_IP = "Your server ip"
+RCON_PORT =
+RCON_PASS = "Your pass"
+
+def send_rcon(cmd):
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(5)
+        sock.connect((RCON_IP, RCON_PORT))
+        
+        # Auth
+        auth_pkt = struct.pack('<ii', 1, 3) + RCON_PASS.encode() + b'\x00\x00'
+        sock.sendall(struct.pack('<i', len(auth_pkt)) + auth_pkt)
+        sock.recv(4096) # Ignore auth response
+        
+        # Command
+        cmd_pkt = struct.pack('<ii', 2, 2) + cmd.encode() + b'\x00\x00'
+        sock.sendall(struct.pack('<i', len(cmd_pkt)) + cmd_pkt)
+        sock.recv(4096) # Receive response
+        sock.close()
+    except:
+        pass # Silent if the server has already crashed
+
+if __name__ == "__main__":
+    # We warn the players to save an restart
+        send_rcon("restart 1")
+        time.sleep(30)
+        send_rcon("admin Reboot server in 30 Secondes!")
+        time.sleep(20)
+        send_rcon("admin Reboot server in 10 Secondes!")
+        send_rcon("save")
+        
+        
